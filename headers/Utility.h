39a4c77fc5d25bc2a6da9456a7145aa1e166bfc8
Diego Castellanos <dac392@scarletmail.rutgers.edu>
2023-11-13 13:53:15 -0500

    added more functionality to Utility class; concider moving all random related events to this class

--- headers/Utility.h
+++ headers/Utility.h
@@ -1,11 +1,27 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#include <string>
+#include <vector>
 #include <utility>
+
 
 class Utility {
 public:
     static std::pair<int, int> generateCoordinate();
+    static std::string hashFunction(const std::pair<int, int>& p);
+    static int heuristic(std::pair<int, int>& a, std::pair<int, int>& b);
+    static std::pair<int, int> shufflePositions(std::vector<std::pair<int, int>>& positions);
+    static void removePosition(std::vector<std::pair<int, int>>& vec, const std::pair<int, int>& elem);
+};
+
+// PairHash definition
+struct PairHash {
+    size_t operator()(const std::pair<int, int>& p) const {
+        auto hash1 = std::hash<int>{}(p.first);
+        auto hash2 = std::hash<int>{}(p.second);
+        return hash1 ^ (hash2 << 1);
+    }
 };
 
 #endif // UTILITY_H
--- src/Utility.cpp
+++ src/Utility.cpp
@@ -1,6 +1,9 @@
 #include "../headers/Utility.h"
 #include <cstdlib>
 #include <ctime>
+#include <algorithm> // For std::shuffle
+#include <iterator>  // For std::begin, std::end
+#include <random>    // For std::default_random_engine
 
 std::pair<int, int> Utility::generateCoordinate(){
     // Seed the random number generator.
@@ -18,4 +21,38 @@ std::pair<int, int> Utility::generateCoordinate(){
     // Create a vector with the generated coordinates and return it
     std::pair<int,int> coordinate = {x, y};
     return coordinate;
+}
+
+std::string Utility::hashFunction(const std::pair<int, int>& p) {
+    return "(" + std::to_string(p.first) + ", " + std::to_string(p.second) + ")";
+}
+
+int Utility::heuristic(std::pair<int, int>& a, std::pair<int, int>& b) {
+    return std::abs(a.first - b.first) + std::abs(a.second - b.second);
+}
+
+std::pair<int, int> Utility::shufflePositions(std::vector<std::pair<int, int>>& positions) {
+    if(positions.empty()){
+        throw std::runtime_error("Utility::shuffle positions is empty"); // or any other error handling
+    }
+
+    // Initialize a random number generator
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    // Shuffle the positions vector
+    std::shuffle(positions.begin(), positions.end(), g);
+
+    // Assuming you want to return the first element after shuffling
+
+    return positions.front();
+
+}
+
+
+void Utility::removePosition(std::vector<std::pair<int, int>>& vec, const std::pair<int, int>& elem) {
+    auto it = std::find(vec.begin(), vec.end(), elem);
+    if (it != vec.end()) {
+        vec.erase(it);
+    }
 }
